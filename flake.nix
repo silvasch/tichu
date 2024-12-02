4e0bcff97862804e4f95bbb2f543d461fa36d236
Silvan Schmidt <mail@silvanschmidt.ch>
2024-12-02 15:12:30 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = { systems, nixpkgs, nixpkgs-unstable, ... }:
+    let
+      eachSystem = f:
+        nixpkgs.lib.genAttrs (import systems) (system:
+          let
+            overlay-unstable = final: prev: {
+              unstable = nixpkgs-unstable.legacyPackages.${prev.system};
+            };
+          in f (import nixpkgs {
+            inherit system;
+            overlays = [ overlay-unstable ];
+          }));
+    in {
+      devShell = eachSystem (pkgs:
+        pkgs.mkShell { packages = with pkgs; [ jdk21 jdt-language-server ]; });
+    };
+}
