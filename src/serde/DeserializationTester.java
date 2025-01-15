261416185bfa436578bcf65e596e644ebdf44915
Silvan Schmidt <mail@silvanschmidt.ch>
2025-01-15 09:21:30 +0100

    refactor: move rank deserialization test to own function

--- src/serde/DeserializationTester.java
+++ src/serde/DeserializationTester.java
@@ -5,6 +5,10 @@ import src.card.Rank;
 
 public class DeserializationTester {
     public static void main(String[] args) throws Exception {
+        DeserializationTester.rankDeserializationTest();
+    }
+
+    public static void rankDeserializationTest() throws Exception {
         PartialDeserialization<Rank> de = Rank.partialDeserialize(Rank.ACE.serialize() + ",asdf");
         assert de.getResult().equals(Rank.ACE);
         assert de.getRemainder().equals(",asdf");
