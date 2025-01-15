c496612e3fd9cb438f78faed7520ffb77b1c2cf6
Silvan Schmidt <mail@silvanschmidt.ch>
2025-01-15 09:24:12 +0100

    feat: deserialize suits

--- src/serde/DeserializationTester.java
+++ src/serde/DeserializationTester.java
@@ -2,6 +2,7 @@
 package src.serde;
 
 import src.card.Rank;
+import src.card.Suit;
 
 public class DeserializationTester {
     public static void main(String[] args) throws Exception {
@@ -11,6 +12,12 @@ public class DeserializationTester {
     public static void rankDeserializationTest() throws Exception {
         PartialDeserialization<Rank> de = Rank.partialDeserialize(Rank.ACE.serialize() + ",asdf");
         assert de.getResult().equals(Rank.ACE);
+        assert de.getRemainder().equals(",asdf");
+    }
+
+    public static void suitDeserializationTest() throws Exception {
+        PartialDeserialization<Suit> de = Suit.partialDeserialize(Suit.RED.serialize() + ",asdf");
+        assert de.getResult().equals(Suit.RED);
         assert de.getRemainder().equals(",asdf");
     }
 }
