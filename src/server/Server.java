9070a43a755683f6816be71ae8219433821abbbc
Silvan Schmidt <mail@silvanschmidt.ch>
2025-01-20 15:03:18 +0100

    fix: formatting

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,5 +1,10 @@
 package src.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.Collections;
 import src.card.Card;
 import src.card.NormalCard;
 import src.card.Rank;
@@ -11,122 +16,113 @@ import src.move.combination.PairCombination;
 import src.move.combination.TripleCombination;
 import src.serde.SerializationException;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Arrays;
-import java.util.Collections;
-
 public class Server {
 
-    private ServerSocket socket;
+  private ServerSocket socket;
 
-    private Team teamOne;
-    private Team teamTwo;
+  private Team teamOne;
+  private Team teamTwo;
 
-    public Server(int port)
-            throws IOException, InvalidCombinationException, SerializationException {
-        this.socket = new ServerSocket(port);
+  public Server(int port) throws IOException, InvalidCombinationException, SerializationException {
+    this.socket = new ServerSocket(port);
 
-        Socket socketOne = this.acceptConnection();
-        Socket socketTwo = this.acceptConnection();
-        Socket socketThree = this.acceptConnection();
-        Socket socketFour = this.acceptConnection();
+    Socket socketOne = this.acceptConnection();
+    Socket socketTwo = this.acceptConnection();
+    Socket socketThree = this.acceptConnection();
+    Socket socketFour = this.acceptConnection();
 
-        Card[][] hands = this.generateHands();
+    Card[][] hands = this.generateHands();
 
-        this.teamOne = new Team(socketOne, socketThree, hands[0], hands[1]);
-        this.teamTwo = new Team(socketTwo, socketFour, hands[2], hands[3]);
+    this.teamOne = new Team(socketOne, socketThree, hands[0], hands[1]);
+    this.teamTwo = new Team(socketTwo, socketFour, hands[2], hands[3]);
 
-        for (Player player : this.getPlayers()) {
-            player.informOfStart();
-        }
-
-        Move move =
-                new FullHouseCombination(
-                        new TripleCombination(
-                                new NormalCard(Suit.BLACK, Rank.SEVEN),
-                                new NormalCard(Suit.RED, Rank.SEVEN),
-                                new NormalCard(Suit.BLUE, Rank.SEVEN)),
-                        new PairCombination(
-                                new NormalCard(Suit.RED, Rank.JACK),
-                                new NormalCard(Suit.BLUE, Rank.JACK)));
-
-        for (Player player : this.getPlayers()) {
-            player.informOfMove(move, "Player one");
-        }
-
-        this.teamOne.informOfEnd(true, this.teamTwo.getPoints());
-        this.teamTwo.informOfEnd(false, this.teamOne.getPoints());
+    for (Player player : this.getPlayers()) {
+      player.informOfStart();
     }
 
-    private Socket acceptConnection() throws IOException {
-        Socket socket = this.socket.accept();
-        System.out.println("got a connection.");
-        return socket;
+    Move move =
+        new FullHouseCombination(
+            new TripleCombination(
+                new NormalCard(Suit.BLACK, Rank.SEVEN),
+                new NormalCard(Suit.RED, Rank.SEVEN),
+                new NormalCard(Suit.BLUE, Rank.SEVEN)),
+            new PairCombination(
+                new NormalCard(Suit.RED, Rank.JACK), new NormalCard(Suit.BLUE, Rank.JACK)));
+
+    for (Player player : this.getPlayers()) {
+      player.informOfMove(move, "Player one");
     }
 
-    private Card[][] generateHands() {
-        Card[] cards = new Card[] {};
-        for (Suit suit : Suit.values()) {
-            for (Rank rank : Rank.values()) {
-                Card card = new NormalCard(suit, rank);
-                cards = Arrays.copyOf(cards, cards.length + 1);
-                cards[cards.length - 1] = card;
-            }
-        }
-        Collections.shuffle(Arrays.asList(cards));
+    this.teamOne.informOfEnd(true, this.teamTwo.getPoints());
+    this.teamTwo.informOfEnd(false, this.teamOne.getPoints());
+  }
 
-        Card[][] hands = new Card[][] {};
+  private Socket acceptConnection() throws IOException {
+    Socket socket = this.socket.accept();
+    System.out.println("got a connection.");
+    return socket;
+  }
 
-        int chunkSize = Math.floorDiv(cards.length, 4);
-        for (int i = 0; i < cards.length; i += chunkSize) {
-            hands = Arrays.copyOf(hands, hands.length + 1);
-            hands[hands.length - 1] =
-                    Arrays.copyOfRange(cards, i, Math.min(cards.length, i + chunkSize));
-        }
+  private Card[][] generateHands() {
+    Card[] cards = new Card[] {};
+    for (Suit suit : Suit.values()) {
+      for (Rank rank : Rank.values()) {
+        Card card = new NormalCard(suit, rank);
+        cards = Arrays.copyOf(cards, cards.length + 1);
+        cards[cards.length - 1] = card;
+      }
+    }
+    Collections.shuffle(Arrays.asList(cards));
 
-        return hands;
+    Card[][] hands = new Card[][] {};
+
+    int chunkSize = Math.floorDiv(cards.length, 4);
+    for (int i = 0; i < cards.length; i += chunkSize) {
+      hands = Arrays.copyOf(hands, hands.length + 1);
+      hands[hands.length - 1] = Arrays.copyOfRange(cards, i, Math.min(cards.length, i + chunkSize));
     }
 
-    private Player[] getPlayers() {
-        return new Player[] {
-            this.teamOne.getPlayerOne(),
-            this.teamOne.getPlayerTwo(),
-            this.teamTwo.getPlayerOne(),
-            this.teamTwo.getPlayerTwo(),
-        };
+    return hands;
+  }
+
+  private Player[] getPlayers() {
+    return new Player[] {
+      this.teamOne.getPlayerOne(),
+      this.teamOne.getPlayerTwo(),
+      this.teamTwo.getPlayerOne(),
+      this.teamTwo.getPlayerTwo(),
+    };
+  }
+
+  private void close() throws IOException {
+    for (Player player : this.getPlayers()) {
+      player.close();
     }
 
-    private void close() throws IOException {
-        for (Player player : this.getPlayers()) {
-            player.close();
-        }
+    this.socket.close();
+  }
 
-        this.socket.close();
+  public static void main(String[] args) throws IOException {
+    int port = -1;
+
+    try {
+      port = Integer.parseInt(args[0]);
+    } catch (IndexOutOfBoundsException e) {
+      if (port == -1) {
+        port = 3000;
+      }
     }
 
-    public static void main(String[] args) throws IOException {
-        int port = -1;
+    System.out.println(String.format("starting the server on '%s'.", port));
 
-        try {
-            port = Integer.parseInt(args[0]);
-        } catch (IndexOutOfBoundsException e) {
-            if (port == -1) {
-                port = 3000;
-            }
-        }
-
-        System.out.println(String.format("starting the server on '%s'.", port));
-
-        Server server = null;
-        try {
-            server = new Server(port);
-        } catch (Exception e) {
-            if (server != null) {
-                server.close();
-            }
-            e.printStackTrace(System.out);
-        }
+    Server server = null;
+    try {
+      server = new Server(port);
+    } catch (Exception e) {
+      if (server != null) {
+        server.close();
+      }
+      e.printStackTrace(System.out);
     }
+  }
 }
