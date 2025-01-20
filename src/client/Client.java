00bcf27e06c72021096753ee5137271693822de7
Silvan Schmidt <mail@silvanschmidt.ch>
2025-01-20 09:15:38 +0100

    feat: make connections

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,7 +1,52 @@
 package src.client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client {
-    public static void main(String[] args) {
-        System.out.println("Hello, Client!");
+
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public Client(String ip, int port) throws IOException {
+        this.socket = new Socket(ip, port);
+        this.out = new PrintWriter(this.socket.getOutputStream(), true);
+        this.in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+        System.out.println("connected.");
+
+        while (true) {
+        }
+    }
+
+    public void close() throws IOException {
+        this.in.close();
+        this.out.close();
+        this.socket.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        String ip = null;
+        int port = -1;
+
+        try {
+            ip = args[0];
+            port = Integer.parseInt(args[1]);
+        } catch (IndexOutOfBoundsException e) {
+            if (ip == null) {
+                ip = "localhost";
+            }
+
+            if (port == -1) {
+                port = 3000;
+            }
+        }
+
+        System.out.println(String.format("connecting to '%s:%s'.", ip, port));
+
+        new Client(ip, port);
     }
 }
---
+++ src/server/Player.java
@@ -0,0 +1,26 @@
+package src.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Player {
+
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public Player(Socket socket) throws IOException {
+        this.socket = socket;
+        this.out = new PrintWriter(this.socket.getOutputStream(), true);
+        this.in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+    }
+
+    public void close() throws IOException {
+        this.in.close();
+        this.out.close();
+        this.socket.close();
+    }
+}
